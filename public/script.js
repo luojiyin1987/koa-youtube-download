14158feb64031006a31a7060a7371b5b94ff6e2e
luojiyin <luojiyin@hotmail.com>
2022-06-23 15:47:14 +0800

    feat: get finish info

--- public/script.js
+++ public/script.js
@@ -2,34 +2,35 @@ const downloadBtn = document.querySelector(".download-button");
 const URLinput = document.querySelector(".URL-input");
 const goFinishBtn = document.querySelector(".ok-button");
 
-
 downloadBtn.addEventListener("click", () => {
-    console.log(`URL: ${URLinput.value}`);
-    sendURL(URLinput.value);
+  console.log(`URL: ${URLinput.value}`);
+  sendURL(URLinput.value);
 });
 
 goFinishBtn.addEventListener("click", () => {
-    goToFinish();
+  goToFinish();
 });
 
-
 function goToFinish() {
-    window.location.href = window.location.href + 'downloaded';
+  window.location.href = window.location.href + "downloaded";
 }
 
 function sendURL(URL) {
-    let xhr = new XMLHttpRequest();
-    xhr.open("POST", "download", true);
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", "download", true);
 
-    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+  xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
 
-    xhr.complete = function (text) {
-        alert(text.responseText);
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState == XMLHttpRequest.DONE) {
+      if (xhr.responseText == 1) {
+        alert("download finished");
+      } else {
+        alert(xhr.responseText);
+      }
     }
+  };
 
-    const data = { URL: URL };
-    xhr.send(JSON.stringify(data));
-
-
-
+  const data = { URL: URL };
+  xhr.send(JSON.stringify(data));
 }
